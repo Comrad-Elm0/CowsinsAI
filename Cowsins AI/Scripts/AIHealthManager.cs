df17e9f05e0a207b76837e1ad3d0acfecf1452ef
dylangobey <dylangobey@outlook.com>
2023-05-14 13:49:32 +0100

    Added Support for Wandering

---
+++ Cowsins AI/Scripts/AIHealthManager.cs
@@ -0,0 +1,40 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AIHealthManager : EnemyAI
+{
+    bool isDead = false;
+
+    public override void Damage(float damage)
+    {
+        if (isDead) return;
+        base.Damage(damage);
+    }
+
+    public override void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+        events.OnDeath.Invoke();
+
+        if (shieldSlider != null) shieldSlider.gameObject.SetActive(false);
+        if (healthSlider != null) healthSlider.gameObject.SetActive(false);
+
+        if (UI.GetComponent<UIController>().displayEvents)
+        {
+            UI.GetComponent<UIController>().AddKillfeed(name);
+        }
+
+        base.Die();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void Start()
+    {
+        base.Start();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# CowsinsAI
+#FPS-Engine-AI
+##What is this asset?
+This asset is an unofficial release of an AI engine that's made for the FPS Engine by Cowsins.
+
+##What are the current abilities of this asset?
+This asset currently has the ability to:
+
+- Directly attack any player using FPS Engine
+- Both Melee and Shooter options
+- Included Demo
+- Constant Updates
+
+##Any bugs?
+Join the Cowsins Discord!
+
+###Version Number
+0.1.0
+
